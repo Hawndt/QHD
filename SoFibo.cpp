56b663d9d4bef78616249091bebe6a30e9269ceb
Hawndt <90140711+Hawndt@users.noreply.github.com>
2022-03-30 15:35:19 +0700

    Add files via upload

---
+++ SoFibo.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#define N 100000
+#define M 1E9 + 7
+#define ll long long
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	ll q , a[N];
+	
+	cin >> q;
+	a[0] = 1 ; a[1] = 1;
+	
+	for(int i=2 ; i<N ; i++) a[i] = (a[i-1] + a[i-2]) % ll(M);
+	
+	while(q--){
+	    ll n;
+	    cin >> n;
+	    cout << a[n-1]<< endl;
+	}
+	
+}
